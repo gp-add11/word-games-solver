984445512d294e06c90acaa1213c9636ae9c25d3
Girish <iitkgp.girish@gmail.com>
2022-01-29 00:59:09 +0100

    Added position eliminator function to optimize solution space

--- wordle/utils/aux_functions.py
+++ wordle/utils/aux_functions.py
@@ -46,25 +46,17 @@ def contains_letters(letters: str, words_list: list) -> np.array:
                                             [char in word for word in words_list])
         return curr_bool_array
 
-        
+
 def set_position_letters(positioned_letters: str, words_list: list) -> np.array:
     curr_bool_array = np.ones(shape = (len(words_list),), dtype = bool) # array of all True
-    if len(positioned_letters) > 5:
-        raise ValueError("More than 5-character string received")
-    elif len(positioned_letters) < 5:
-        positioned_letters = positioned_letters + ('_'*(5-len(positioned_letters)))
-    positioned_letters = positioned_letters.replace(' ', '_')
     for idx, char in enumerate(positioned_letters):
         if char == '_':
             continue
-        elif char in alphabets:
+        else:
             curr_bool_array = and_operation(curr_bool_array,
                                             [word[idx] == char for word in words_list])
-        else: # Redundant since input values are already processed
-            raise ValueError("Unknown character: " + char)
-            break
-    return curr_bool_array
-    
+    return curr_bool_array    
+
 
 def doesnt_contain_letters(not_matching_letters: str, words_list: list) -> np.array:
     curr_bool_array = np.ones(shape = (len(words_list),), dtype = bool) # array of all True
@@ -99,13 +91,19 @@ def calculate_guess_feedback(guessed_words: list, positioned_letters: str, match
     
     return contains_letters(''.join(all_matched_letters), words_list) \
             & doesnt_contain_letters(''.join(unmatched_letters), words_list) \
-            & set_position_letters(positioned_letters, words_list)    
+            & set_position_letters(positioned_letters, words_list) \
+            & wrong_position_eliminator(guessed_words, positioned_letters, matched_letters, words_list)
             
 
 def user_input() -> tuple:
     guessed_words = input_cleaner(input('Enter guessed words separated by space:\n')).split()
     matched_letters = input_cleaner(input('Enter letters that have successful match so far:\n'))
     positioned_letters = input_cleaner(input('Enter letters whose position is known (replace unknown position values with underscore):\n'))
+    if len(positioned_letters) > 5:
+        raise ValueError("More than 5-character string received")
+    elif len(positioned_letters) < 5:
+        positioned_letters = positioned_letters + ('_'*(5-len(positioned_letters)))
+    positioned_letters = positioned_letters.replace(' ', '_')
     return guessed_words, matched_letters, positioned_letters
     
     
@@ -122,4 +120,34 @@ def get_n_letter_words(file:str, num_char:int = 5):
         words = f.read()
     words = words.lower().split('\n')
     return list(filter(lambda word: len(word) == num_char, words))
+
+
+def wrong_position_eliminator(guessed_words: list, positioned_letters: str, matched_letters: str, words_list: list) -> np.array:
+    '''
+    # Docstring may need update due to changes to the function.
+    Function to use letters which are identified to be present in the word but with unknown position. 
+    That means the position at which the letter is guessed is incorrect; (word_length - 1) other possible positions remain.
+    Therefore the words having the identified letter inputted at a particular position should be eliminated from the words_list as possible solution.
+    There should be a separate logic outside the function to extract the identified letter and it's incorrectly guessed position.
+    
+    Input:
+        
+        
+    Returns:
+        np.array of boolean elements against words_list
+    '''
+    letter_positions = []
+    for idx in np.where([not x.isalpha() for x in positioned_letters])[0]: #Get indices of all underscores
+        for word in guessed_words:
+            word_idxth_letter = word[idx]
+            if (word_idxth_letter in matched_letters) and (word_idxth_letter != positioned_letters[idx]): #Second part could be redundant
+                letter_positions.append((idx, word_idxth_letter))    
+    curr_bool_array = np.ones(shape = (len(words_list),), dtype = bool) # array of all True
+    if len(letter_positions) == 0:
+        return curr_bool_array
+    else:
+        for position, letter in letter_positions:
+            curr_bool_array = and_operation(curr_bool_array,
+                                            [word[position] != letter for word in words_list])
+        return curr_bool_array
     
